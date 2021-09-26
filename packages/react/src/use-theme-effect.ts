6b870af95650c23c8456ea0e8b491bcfb81b2672
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-09-26 16:54:23 -0500

    chore: rename to react

--- packages/react/src/use-theme-effect.ts
+++ packages/react/src/use-theme-effect.ts
@@ -5,20 +5,18 @@ type Themes = 'light-theme' | 'dark-theme';
 
 const isSSR = typeof document === 'undefined';
 
+const noop = () => undefined;
+const useSafeLayoutEffect = isSSR ? noop : useLayoutEffect;
+
 export const useThemeEffect = (defaultTheme: Themes = 'light-theme') => {
   const htmlTag = !isSSR ? document.getElementsByTagName('html')[0] : undefined;
   const hasThemeClass = htmlTag?.classList.contains(themeClass);
   const hasModeClass =
     htmlTag?.classList.contains('light-theme') || htmlTag?.classList.contains('dark-theme');
 
-  useLayoutEffect(() => {
-    if (isSSR) return;
-    if (!htmlTag) return;
-    if (!hasThemeClass) {
-      htmlTag.classList.add(themeClass);
-    }
-    if (!hasModeClass) {
-      htmlTag.classList.add(defaultTheme);
-    }
+  useSafeLayoutEffect(() => {
+    if (isSSR || !htmlTag) return;
+    if (!hasThemeClass) htmlTag.classList.add(themeClass);
+    if (!hasModeClass) htmlTag.classList.add(defaultTheme);
   }, [isSSR, htmlTag, hasModeClass, hasThemeClass]);
 };
